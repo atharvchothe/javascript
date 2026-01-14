ead0bce328fc5b44bcbde96c35dc8f1ac6630708
Atharv Chothe <atharvchothe839@gmail.com>
2026-01-15 01:28:42 +0530

    programs

---
+++ operation.js
@@ -0,0 +1,39 @@
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+
+
+
+
+let str1 = "hello ";
+let str2 = "Atharv ";
+console.log(str1 + str2 );
+
+
+console.log(1+"2");
+console.log( "1"+ 2+2 );
+
+console.log(1+2+"2");
+
+
+// js only auto convert a strin to nums in arith opreatiob
+
+// find the reason behind 
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+let k_count =100;
+k_count++;
+console.log(k_count);
+
+
+
+let score_conter= 200 ;
+++score_conter;
+console.log(score_conter);
+
+
---
+++ varible.js
@@ -0,0 +1,21 @@
+
+var a =10;
+let ab =20;
+
+
+console.log(a)
+console.log(ab)
+{
+    var a =200;
+    let ab=100;
+    console.log(a)
+    console.log(ab)
+}
+console.log("var "+a)
+console.log("let " +ab)
+
+
+
+let no =33;
+let no_string  = String(no);
+console.log(no_string); 
