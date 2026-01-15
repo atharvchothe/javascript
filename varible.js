5a9311bc0331bd92373da45532c10ce939235745
Atharv Chothe <atharvchothe839@gmail.com>
2026-01-15 22:57:09 +0530

    modified

--- varible.js
+++ varible.js
@@ -2,6 +2,33 @@
 var a =10;
 let ab =20;
 
+//  data types 
+
+let x;
+console.log(x); //  x = undefined 
+
+let y = BigInt("123")
+console.log(typeof(y));
+console.log("BigInt = " + y);
+
+let z = Symbol("Hello")
+console.log(z);
+
+
+const student ={
+    name : "Abhi",
+    age : 20 ,
+    cgpa : 8.5,
+};
+
+console.log(student);
+console.log(typeof(student));
+console.log(student["name"]);
+student.name;
+
+
+
+
 
 console.log(a)
 console.log(ab)
